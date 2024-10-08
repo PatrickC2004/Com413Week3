577f1726471feaa3c01b0ac8477f25d981ec0f65
Patrick Campbell <csf23cap>
2024-10-08 10:11:26 +0100

    intial commit

--- library/Program.cs
+++ library/Program.cs
@@ -3,25 +3,32 @@
     string Title;
     string Author;
     string ISBN;
+    int NoPages;
+
+    void DisplayInfo()
+    {
+        Console.WriteLine("Book Information \n ------------");
+        Console.WriteLine($"Book title {Title},\nBook author {Author}, \nBook ISBN {ISBN}, \nNumber of pages {NoPages}");  //print book
+        Console.WriteLine();
+    }
 
     static void Main(string[] args)
     {
         //Create new instance of book class
         Book book = new Book();
-
         book.Title = "C# for biggeners";
         book.Author = "Bill Gates";
         book.ISBN = "12345678";
+        book.NoPages = 200;
 
         Book book2 = new Book();
-
         book2.Title = "C# Methods and Classes";
         book2.Author = "Microsoft";
         book2.ISBN = "153561";
+        book2.NoPages = 50;
 
-        //Output book information
-        Console.WriteLine($"Book title {book.Title}, Book author {book.Author}, Book ISBN {book.ISBN}");  //print first book
-        Console.WriteLine($"Book title {book2.Title}, Book author {book2.Author}, Book ISBN {book2.ISBN}"); //print second book
-
+        book.DisplayInfo();
+        book2.DisplayInfo();
+       
     }
 }
